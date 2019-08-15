81c1b878673445b90aee971c795d694ab729b0dd
Eric Diaz <ericdiaz@pursuit.org>
2019-08-15 16:37:25 -0400

    created basic sqliteopenhelper db

---
+++ app/src/main/java/ericdiaz/program/gotennachallenge/db/PlacesDatabase.java
@@ -0,0 +1,95 @@
+package ericdiaz.program.gotennachallenge.db;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ericdiaz.program.gotennachallenge.model.Place;
+
+public class PlacesDatabase extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "PlacesDatabase.db";
+    private static final String TABLE_NAME = "TransactionTable";
+    private static final int SCHEMA = 1;
+    private static PlacesDatabase singleDatabaseInstance;
+    private SQLiteDatabase db;
+
+    private PlacesDatabase(@NonNull final Context context) {
+        super(context, DATABASE_NAME, null, SCHEMA);
+    }
+
+    public static PlacesDatabase getSingleDatabaseInstance(@NonNull final Context context) {
+        if (singleDatabaseInstance == null) {
+            singleDatabaseInstance = new PlacesDatabase(context);
+        }
+        return singleDatabaseInstance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME +
+          " (" +
+          "_id INTEGER PRIMARY KEY, " +
+          "name TEXT, " +
+          "latitude REAL, " +
+          "longitude REAL," +
+          "description TEXT" +
+          ");");
+    }
+
+    public void insertPlace(@NonNull final Place place) {
+        Cursor cursor = getReadableDatabase().rawQuery(
+          "SELECT * FROM " + TABLE_NAME +
+            " WHERE _id = '" + place.getId(),
+          null);
+        if (cursor.getCount() == 0) {
+            getWritableDatabase().execSQL("INSERT INTO " + TABLE_NAME +
+              "(_id, name, latitude, longitude, description) " +
+              "VALUES('" +
+              place.getId() + "', '" +
+              place.getName() + "', '" +
+              place.getLatitude() + "', '" +
+              place.getLongitude() + "', '" +
+              place.getDescription() + "', '" +
+              "');");
+        }
+        cursor.close();
+    }
+
+    public List<Place> getAllPlaces() {
+        List<Place> allPlaces = new ArrayList<>();
+        Cursor cursor = getReadableDatabase().rawQuery(
+          "SELECT * FROM " + TABLE_NAME + ";", null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    allPlaces.add(new Place(
+                      cursor.getInt(cursor.getColumnIndex("_id")),
+                      cursor.getString(cursor.getColumnIndex("name")),
+                      cursor.getDouble(cursor.getColumnIndex("latitude")),
+                      cursor.getDouble(cursor.getColumnIndex("longitude")),
+                      cursor.getString(cursor.getColumnIndex("description"))
+                    ));
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
+        return allPlaces;
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        this.db = db;
+        super.onOpen(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
